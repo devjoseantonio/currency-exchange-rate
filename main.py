c97166fa06825e98f6cea01df318e282eb1cb654
devjoseantonio <tonysoftco@gmail.com>
2023-10-31 11:11:25 -0500

    Code refactor and response added

--- main.py
+++ main.py
@@ -1,57 +1,53 @@
 import requests
 import json
+import env
 
-option = 0
+currenys = ["EUR", "USD", "COP"]
+option1 = 0
+option2 = 0
 headers = {"accept": "application/json"}
 
-##print(response.text)
-print("This program converts one currency to another.")
-print("Select an option:\n"
-      "1. EUR to USD\n"
-      "2. EUR to COP\n"
-      "3. USD to EUR\n"
-      "4. USD to COP\n"
-      "5. COP to USD\n"
-      "6. COP to EUR\n")
-while option != '':
+def printOptions ():
+    i = 0
+    for x in currenys:
+        print(str(i) + ". " + x)
+        i += 1
+
+
+while option1 != '':
     error = False
-    option = input("Insert a number or press enter to exit: ")
-    if option != '':
+    print("\nThis program converts one currency to another.")
+    print("Select an option to convert from.")
+    printOptions()
+    option1 = input("Insert a number or press enter to exit: ")
+    if option1 != '':
+        
         try:
-            option = int(option)
-            match option:
-                case 1:
-                    currencyFrom = "EUR"
-                    currencyTo = "USD"
-                case 2:
-                    currencyFrom = "EUR"
-                    currencyTo = "COP"
-                case 3:
-                    currencyFrom = "USD"
-                    currencyTo = "EUR"
-                case 4:
-                    currencyFrom = "USD"
-                    currencyTo = "COP"
-                case 5:
-                    currencyFrom = "COP"
-                    currencyTo = "USD"
-                case 6:
-                    currencyFrom = "COP"
-                    currencyTo = "EUR"
-                case _:
-                    print("Error: the number {} is not a valid option\n".format(option))
-                    error = True
-            if not error:
-                amount = float(input("Insert the amount to convert: "))
-                if amount <= 0:
-                    print("Error: The amount must be greather than 0")
-                else:
-                    url = "https://api.fastforex.io/convert?" \
-                    "from=" + currencyFrom + "&to=" + currencyTo + "&amount={}" \
-                    "&api_key=".format(amount)
+            option1 = int(option1)
+            if 0 <= option1 <= 2:
+                print("Select an option to convert to.")
+                printOptions()
+                option2 = input("Insert a number or press enter to return: ")
+                option2 = int(option2)
+                if 0 <= option2 <= 2:
+                    print("Select an amount to convert.")
+                    amount = float(input("Insert the amount to convert: "))
+                    if amount <= 0:
+                        print("Error: The amount must be greather than 0")
+                    else:
+                        url = ("https://api.fastforex.io/convert?" \
+                            "from=" + currenys[option1] + "&to=" + currenys[option2] + "&amount={}" \
+                                "&api_key=" + env.API_KEY).format(amount)
                     response = requests.get(url, headers=headers)
                     json_response = json.loads(response.text)
+                    response = json_response.get("result")
                     
+                    print(str(amount) + " " + currenys[option1] + " in " + currenys[option2] + " are " + str(response.get(currenys[option2])))
+                else:
+                    error = True
+            else:
+                error = True
+            if error:
+                print("Error: The number must be in between of 0 and 2")
         except:
             print("Error: The software only accepts numbers\n")
-            
